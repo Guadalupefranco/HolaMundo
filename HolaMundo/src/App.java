276c6c63b82633acc8f867053fd482ab2c4a78c7
Guadalupefranco <91037205+Guadalupefranco@users.noreply.github.com>
2022-02-23 20:22:01 -0600

    Update App.java

--- HolaMundo/src/App.java
+++ HolaMundo/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Detalles del equipo Zugus:");
-        System.out.println("Eric Zurita Vazquez - UPIICSA IPN");
+        System.out.println("María Guadalupe Franco Rodríguez - ESCOM IPN");
     }
 }
